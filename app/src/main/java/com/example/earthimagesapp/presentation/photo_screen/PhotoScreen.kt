69a565532900ea09b6552fe0bc76abd03e00c63e
Mateo Rial <mateo.rial@perficient.com>
2023-03-29 20:16:18 -0300

    Add pan and zoom to photo

--- app/src/main/java/com/example/earthimagesapp/presentation/photo_screen/PhotoScreen.kt
+++ app/src/main/java/com/example/earthimagesapp/presentation/photo_screen/PhotoScreen.kt
@@ -1,18 +1,27 @@
 package com.example.earthimagesapp.presentation.photo_screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -20,6 +29,8 @@ import coil.request.ImageRequest
 import com.example.earthimagesapp.util.DateUtils
 import com.example.earthimagesapplication.R
 import com.ramcosta.composedestinations.annotation.Destination
+import java.lang.Math.*
+import kotlin.math.roundToInt
 
 @Composable
 @Destination
@@ -27,18 +38,63 @@ fun PhotoScreen(viewModel: PhotoScreenViewModel = hiltViewModel()) {
 
     val state = viewModel.state
 
-    Box(
-        modifier = Modifier
-            .padding(4.dp)
-            .aspectRatio(1f)
-            .clip(RoundedCornerShape(5.dp))
-            .background(Color.Gray),
-        contentAlignment = Alignment.Center
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        val angle by remember { mutableStateOf(0f) }
+        var zoom by remember { mutableStateOf(1f) }
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
 
+        val configuration = LocalConfiguration.current
+        val screenWidth = configuration.screenWidthDp.dp.value
+        val screenHeight = configuration.screenHeightDp.dp.value
 
-        state.photo?.caption?.let { Text(text = it) }
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(
+                    "https://epic.gsfc.nasa.gov/archive/enhanced/${
+                        state.photo?.let {
+                            DateUtils.formatDateToGetImage(
+                                it.date
+                            )
+                        }
+                    }/png/epic_RGB_${state.photo?.identifier}.png"
+                )
+                .crossfade(true)
+                .build(),
+            contentDescription = "contentDescription",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .graphicsLayer(
+                    scaleX = zoom,
+                    scaleY = zoom,
+                    rotationZ = angle
+                )
+                .pointerInput(Unit) {
+                    detectTransformGestures(
+                        onGesture = { _, pan, gestureZoom, _ ->
+                            zoom = (zoom * gestureZoom).coerceIn(1F..4F)
+                            if (zoom > 1) {
+                                val x = (pan.x * zoom)
+                                val y = (pan.y * zoom)
+                                val angleRad = angle * PI / 180.0
 
+                                offsetX =
+                                    (offsetX + (x * cos(angleRad) - y * sin(angleRad)).toFloat()).coerceIn(
+                                        -(screenWidth * zoom)..(screenWidth * zoom)
+                                    )
+                                offsetY =
+                                    (offsetY + (x * sin(angleRad) + y * cos(angleRad)).toFloat()).coerceIn(
+                                        -(screenHeight * zoom)..(screenHeight * zoom)
+                                    )
+                            } else {
+                                offsetX = 0F
+                                offsetY = 0F
+                            }
+                        }
+                    )
+                }
+                .fillMaxSize()
+        )
     }
-
 }
