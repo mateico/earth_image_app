c5f671a639cb9b085b66dc250f184207a38a08f4
Mateo Rial <mateo.rial@perficient.com>
2023-03-29 10:09:31 -0300

    add timber for logs

--- app/src/main/java/com/example/earthimagesapp/EarthImagesApplication.kt
+++ app/src/main/java/com/example/earthimagesapp/EarthImagesApplication.kt
@@ -1,7 +1,16 @@
 package com.example.earthimagesapp
 
 import android.app.Application
+import com.example.earthimagesapplication.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
-class EarthImagesApplication : Application()
+class EarthImagesApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
