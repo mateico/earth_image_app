26327d36f13766fd93d91c3abe85eb509775028a
Mateo Rial <mateo.rial@perficient.com>
2023-03-29 18:05:05 -0300

    show list of images using coil (temporary solution)

--- app/src/main/java/com/example/earthimagesapp/presentation/photo_listing/ImageItem.kt
+++ app/src/main/java/com/example/earthimagesapp/presentation/photo_listing/ImageItem.kt
@@ -1,17 +1,21 @@
 package com.example.earthimagesapp.presentation.photo_listing
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.earthimagesapp.domain.model.Day
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.earthimagesapp.domain.model.ImageData
+import com.example.earthimagesapp.util.DateUtils
 
 @Composable
 fun ImageItem(image: ImageData,
@@ -25,6 +29,20 @@ fun ImageItem(image: ImageData,
         Column(modifier = Modifier
             .padding(12.dp)
             .fillMaxWidth()) {
+            AsyncImage(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+                    .clip(CircleShape),
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("https://epic.gsfc.nasa.gov/archive/enhanced/${DateUtils.formatDateToGetImage(image.date)}/png/epic_RGB_${image.identifier}.png")
+                    .crossfade(true)
+                    .build(),
+                placeholder = painterResource(com.example.earthimagesapplication.R.drawable.placeholder),
+                contentDescription = "This is the description",
+                contentScale = ContentScale.Crop
+
+            )
             Text(text = image.date)
             Text(text = image.caption)
         }
---
+++ app/src/main/java/com/example/earthimagesapp/util/DateUtils.kt
@@ -0,0 +1,15 @@
+package com.example.earthimagesapp.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateUtils {
+
+    fun formatDateToGetImage(date: String): String{
+        val input = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val dateValue: Date = input.parse(date)
+        val output = SimpleDateFormat("yyyy/MM/dd")
+        return output.format(dateValue)
+    }
+
+}
